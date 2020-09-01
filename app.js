6df924fa2cf1de5370fec9c7c2c5b057d03561e6
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-09-01 23:20:34 +0300

    integrates mongodb connection and middleware for response handling

--- app.js
+++ app.js
@@ -12,4 +12,24 @@ app.use(express.urlencoded({ extended: true }));
 require('./routers')(app);
 
 
+require('./core/database/mongo');
+
+
+app.use((err, req, res, next) => {
+    const code = err.code || 500;
+    if (err.code) {
+        res.status(code).json({
+            status: false,
+            ...err,
+        });
+    } else {
+        res.status(code).json({
+            code,
+            status: false,
+            message: 'Something went wrong!',
+            trace: err,
+        });
+    }
+});
+
 app.listen(process.env.PORT, () => console.log(`Application runs on port ${process.env.PORT}`));
