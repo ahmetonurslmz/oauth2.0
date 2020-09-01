60278e745bb6426da0d25afd59a97823c2e0505b
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-09-01 23:19:30 +0300

    create mongo db connection structure

---
+++ core/database/mongo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+require('dotenv').config();
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            autoIndex: true, //this is the code I added that solved it all
+            keepAlive: true,
+        });
+        console.log('MongoDb has been connected.');
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+connectDb()
