ef43ab635e520867418ba2ac7f9632792173e1e8
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-09-02 23:10:21 +0300

    adds check client_url middleware

--- modules/auth/routers/index.js
+++ modules/auth/routers/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 
 
 const { getAccessToken, getAuthorizationCode, createClient } = require('../controllers/authController');
 
-
 router.post('/', (req, res, next) => next(), getAccessToken);
 router.post('/authorization_code', getAuthorizationCode);
-router.post('/client', createClient);
+router.post('/client', [
+    check('client_url').exists()
+], createClient);
 
 module.exports = router;
 
