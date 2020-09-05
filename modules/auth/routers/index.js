d5380baf68dec0cba1e1b2b340ac4ae9e51e3c25
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-09-05 21:57:03 +0300

    adds body validator to access token endpoint

--- modules/auth/routers/index.js
+++ modules/auth/routers/index.js
@@ -5,7 +5,9 @@ const { check } = require('express-validator');
 
 const { getAccessToken, getAuthorizationCode, createClient } = require('../controllers/authController');
 
-router.post('/', (req, res, next) => next(), getAccessToken);
+router.post('/access_token', [check('client_id').exists(),
+    check('code').exists(),
+    check('grant_type').exists()], getAccessToken);
 router.post('/authorization_code', [check('client_id').exists(), check('response_type').exists()], getAuthorizationCode);
 router.post('/client', [
     check('client_url').exists()
