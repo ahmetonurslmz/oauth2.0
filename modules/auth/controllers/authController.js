2aff09c26dafa39c6b0f34ccefb050d378e1c190
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-09-05 17:28:59 +0300

    completes authorization code endpoint and adds body parameter checker to client endpoint

--- modules/auth/controllers/authController.js
+++ modules/auth/controllers/authController.js
@@ -1,5 +1,8 @@
 const { errorResolver, successResolver, validationResolver } = require('../../../core/utils/resolvers');
 const { validationResult } = require('express-validator');
+const Client = require('../models/ClientModel');
+
+const { findOrThrow } = require('../../../core/utils/mongoInterrogator');
 
 
 module.exports.getAccessToken = (req, res) => {
@@ -10,11 +13,54 @@ module.exports.getAccessToken = (req, res) => {
 };
 
 
-module.exports.getAuthorizationCode = (req, res) => {
+module.exports.getAuthorizationCode = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        validationResolver(errors.array(), next);
+    } else {
+        const { client_id, response_type } = req.body;
+
+        try {
+            await Client.init();
+            const requestedClient = await findOrThrow(Client, { _id: client_id, response_type }, 'client');
+
+            if (req.hostname === 'localhost' || requestedClient.client_url === req.hostname) {
+                const crypto = require('crypto');
+                const SECRET = 'password';
+                const ALGORITHM = 'sha256';
+                const hash = crypto.createHmac(ALGORITHM, SECRET).update(Math.random().toString(36)).digest('base64');
 
 
-    res.status(200).json({
-    });
+                const AuthorizationCode = require('../models/AuthorizationCodeModel');
+
+                await AuthorizationCode.init();
+
+                const PERIOD_OF_VALIDITY = 10; //day
+                const newAuthorizationCode = {
+                    client_id,
+                    code: hash,
+                    expired_at: new Date(new Date().getTime()+(PERIOD_OF_VALIDITY*24*60*60*1000)),
+                }
+
+                try {
+                    const { _doc } = await AuthorizationCode.create(newAuthorizationCode);
+                    successResolver(res, {
+                        data: _doc,
+                        message: 'Authorization code generated!',
+                    });
+                } catch (e) {
+                    errorResolver(e, next);
+                }
+            } else {
+                const e = new Error();
+                e.message = 'Invalid client!';
+                e.code = 403;
+                throw e;
+            }
+        } catch (e) {
+            errorResolver(e, next);
+        }
+    }
 };
 
 
@@ -23,19 +69,13 @@ module.exports.createClient = (req, res, next) => {
     if (!errors.isEmpty()) {
         validationResolver(errors.array(), next);
     } else {
-        const crypto = require('crypto');
-        const Client = require('../models/ClientModel');
 
         const { client_url } = req.body;
 
-        const SECRET = 'password';
-        const ALGORITHM = 'sha256';
 
-        const hash = crypto.createHmac(ALGORITHM, SECRET).update(client_url).digest('base64');
 
         const newClient = {
             response_type: 'code',
-            client_id: hash,
             client_url,
         };
 
