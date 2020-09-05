a2a4e7fcc58d712f6305711540bc2660fdcdf2a0
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-09-05 21:55:33 +0300

    creates access token model

---
+++ modules/auth/models/AccessTokenModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const collection = 'access_tokens';
+
+const { Schema } = mongoose;
+
+
+const ModelSchema = new Schema({
+    grant_type: {
+        type: String,
+        required: true,
+    },
+    expires_in: {
+        type: Number,
+        default: 36000,
+    },
+    client_id: {
+        type:  mongoose.ObjectId,
+        required: true,
+    },
+    is_active: {
+        type: Boolean,
+        default: true,
+    },
+    access_token: {
+        type: String,
+        required: true,
+    },
+    refresh_token: {
+        type: String,
+        required: true,
+    },
+    token_type: {
+        type: String,
+        default: 'Bearer',
+    },
+    created_at: {
+        type: Date,
+        default: new Date(),
+    },
+}, { collection, versionKey: false });
+
+
+const model = mongoose.model(collection, ModelSchema);
+module.exports = model
