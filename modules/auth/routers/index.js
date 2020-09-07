b06bb4fc917601192ffbfd799f97bb2b594daa6b
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-09-07 23:00:01 +0300

    formats auth router middlewares

--- modules/auth/routers/index.js
+++ modules/auth/routers/index.js
@@ -3,15 +3,21 @@ const router = express.Router();
 const { check } = require('express-validator');
 
 
-const { getAccessToken, getAuthorizationCode, createClient } = require('../controllers/authController');
+const { getAccessToken, getAuthorizationCode, createClient, generateServerKeys } = require('../controllers/authController');
 
-router.post('/access_token', [check('client_id').exists(),
+router.post('/access_token', [
+    check('client_id').exists(),
     check('code').exists(),
-    check('grant_type').exists()], getAccessToken);
-router.post('/authorization_code', [check('client_id').exists(), check('response_type').exists()], getAuthorizationCode);
+    check('grant_type').exists()
+], getAccessToken);
+router.post('/authorization_code', [
+    check('client_id').exists(),
+    check('response_type').exists()
+], getAuthorizationCode);
 router.post('/client', [
     check('client_url').exists()
 ], createClient);
+router.post('/server_keys', generateServerKeys)
 
 module.exports = router;
 
