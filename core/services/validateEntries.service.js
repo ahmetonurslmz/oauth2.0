9fd54b23eee4bbf551998215facb7f191230226b
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-09-19 23:32:24 +0300

    creates validateEntries service to validate inputs

---
+++ core/services/validateEntries.service.js
@@ -0,0 +1,17 @@
+const validateEntries = (entries, req) => {
+    const data = Array.isArray(entries) ? entries : [entries];
+
+    data.forEach((entry) => {
+        if (!(Object.prototype.hasOwnProperty.call(req.body, entry) && [' ', '', null, false].every(e => e !== req.body[entry]))) {
+            const err = new Error();
+            err.field = entry;
+            err.message = `Couldn't find ${entry}`;
+            err.code = 404;
+            throw err;
+        }
+    });
+
+    return true;
+};
+
+module.exports = validateEntries;
