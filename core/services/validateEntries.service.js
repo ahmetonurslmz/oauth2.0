ece7266585184312f01923c12c2032d3ed85aaac
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-09-19 23:38:09 +0300

    changes validation condition

--- core/services/validateEntries.service.js
+++ core/services/validateEntries.service.js
@@ -2,7 +2,9 @@ const validateEntries = (entries, req) => {
     const data = Array.isArray(entries) ? entries : [entries];
 
     data.forEach((entry) => {
-        if (!(Object.prototype.hasOwnProperty.call(req.body, entry) && [' ', '', null, false].every(e => e !== req.body[entry]))) {
+
+
+        if (!(Object.prototype.hasOwnProperty.call(req.body, entry) && req.body[entry] && req.body[entry].trim())) {
             const err = new Error();
             err.field = entry;
             err.message = `Couldn't find ${entry}`;
