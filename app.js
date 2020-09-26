7356ee7cf1bc6b889d0a21e28a9f887c21fb7c1d
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-09-26 17:30:24 +0300

    creates view engine with ejs

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ dotenv.config();
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname, '/views'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
